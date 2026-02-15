499351dac99447a54e846c9e80055c0341c157a4
Kavishka Rasanjana <krasanjana81@gmail.com>
2026-02-15 22:10:44 +0530

    Updated frontend with Tailwind and added location logic to backend

---
+++ PassengerReportAPI/PassengerReportAPI/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PassengerReportAPI.Models
+{
+    public class Location
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Province { get; set; } = null!;
+
+        public List<string> Districts { get; set; } = null!;
+    }
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,11 @@
-import ViolationForm from './ViolationForm';
+import TransitGuard from './TransitGuard'
 
 function App() {
   return (
-    // CSS CLASSES EXPLAINED:
-    // 1. min-vh-100: Sets the minimum height to 100% of the viewport (Full screen height)
-    // 2. w-100: Sets the width to 100% to ensure the background covers everything
-    // 3. d-flex: Enables Flexbox layout
-    // 4. justify-content-center: Centers the content horizontally (Left to Right)
-    // 5. align-items-center: Centers the content vertically (Top to Bottom)
-    // 6. bg-light: Adds a light gray background color for better contrast
-    <div className="min-vh-100 w-100 d-flex justify-content-center align-items-center bg-light">
-      
-      {/* The Container component handles responsive width and spacing (margins/padding) */}
-      <div className="container">
-        <ViolationForm />
-      </div>
-
-    </div>
-  );
+    <>
+      <TransitGuard />
+    </>
+  )
 }
 
-export default App;
+export default App
